aece2b1b51847cd4f1de3a56e1a0e56beb67bd96
minkyoung kim <minry20415@naver.com>
2024-06-19 22:50:14 +0900

    README 수정

--- src/main/java/boj/ch05_정렬/Q1181.java
+++ src/main/java/boj/ch05_정렬/Q1181.java
@@ -19,7 +19,7 @@ public class Q1181 {
 			@Override
 			public int compare(String o1, String o2) {
 				if (o1.length() == o2.length())
-					return o1.compareTo(o2);
+					return o1.compareTo(o2); // 사전순 정렬
 				return o1.length() - o2.length();
 			}
 		});
